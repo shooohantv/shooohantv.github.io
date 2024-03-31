cc151d536aa7b40b4c09c2f9961f52ae18de6530
shooohantv <165518304+shooohantv@users.noreply.github.com>
2024-04-01 02:44:13 +0600

    Create iframe.php

---
+++ iframe.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html><html><head><style type="text/css">
+  body {
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    width: 100%;
+    height: 100%;
+  }
+
+  .video-embed-container {
+    position: relative;
+    padding-bottom: 56.25%;
+    padding-top: 0px;
+    height: 0;
+    overflow: hidden;
+  }
+
+  .video-embed-container iframe,
+  .video-embed-container object,
+  .video-embed-container embed {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+</style>
+<style type="text/css">* {user-select: auto !important; -webkit-user-select: auto !important;}</style><input type="hidden" id="inject_idm_text_selection"><style></style></head><body>
+<div class="video-embed-container">
+<iframe class="responsive-embed-item" src="" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" sandbox="allow-scripts allow-same-origin" allowfullscreen="true" allow="encrypted-media"></iframe>
+</div>
+
+</body></html>
